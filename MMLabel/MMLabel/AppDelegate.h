d78b414cf69cc07639ab5495a7a654c845fe7ad2
iConfig <com.iScarlett>
2015-11-22 21:55:40 +0800

    test

---
+++ MMLabel/MMLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMLabel
+//
+//  Created by 鸟不拉屎大王 on 15/11/22.
+//  Copyright © 2015年 com.iScarlett.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MMLabel/MMLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MMLabel
+//
+//  Created by 鸟不拉屎大王 on 15/11/22.
+//  Copyright © 2015年 com.iScarlett.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
